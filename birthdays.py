62d90f07b11a6892a62014fffc32893332b4c854
Ilhame Sebbar <sebbarilhame@gmail.com>
2023-11-15 17:00:04 +0100

    Commented module goal and functions

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,6 @@
+'''The goal of this module is to define two functions to create a dictionary and 
+understand the birthday of the celebreties'''
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +9,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+    '''Functions which returns the name of the celebrities prenest in the variable birthdays'''
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+    '''Function which returns the name and the corrispant birthday'''
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
